13bd04ec7e5aaacb7b366780f17890d52a56416a
Rawgher <rdpender3@gmail.com>
2024-08-07 14:23:21 -0700

    finished exercises

---
+++ markov.js
@@ -0,0 +1,64 @@
+/** Textual markov chain generator */
+
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter(c => c !== "");
+    this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    let chains = new Map();
+
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (chains.has(word)) chains.get(word).push(nextWord);
+      else chains.set(word, [nextWord]);
+    }
+
+    this.chains = chains;
+  }
+
+
+  /** return random text from chains */
+
+  static choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)];
+  }
+
+  makeText(numWords = 100) {
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+
+    return out.join(" ");
+  }
+}
+
+// let mm = new MarkovMachine("the cat in the hat");
+
+// let one = mm.makeText()
+// let two = mm.makeText(numWords=50)
+
+// console.log(one)
+// console.log(two)
+
+module.exports = {
+  MarkovMachine
+}
